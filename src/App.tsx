f75b1043dc9e4e4cef766147ff01f1a371504086
matias <matias.tarmo@aalto.fi>
2024-01-10 21:52:25 +0200

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/board/:id" element={<BoardContainer/>} />
       </Routes>
     </Router>
